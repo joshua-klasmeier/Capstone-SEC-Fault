c35a78489555277ed1f34c3861f06672f15b9708
Connor Allen <connorallenj658@gmail.com>
2026-02-25 14:29:07 -0500

    Additional Endpoints for Swagger API Schema

--- backend/main.py
+++ backend/main.py
@@ -12,8 +12,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Unsure how exactly logging in works with Google. Below request might need changed
+class LoginRequest(BaseModel):
+    id_token: str
 
-class ChatRequest(BaseModel):
+class SignoutRequest(BaseModel):
+    userId: int
+
+class NewChatRequest(BaseModel):
+    name: str
+
+class NewMsgRequest(BaseModel):
     message: str
 
 
@@ -21,7 +30,22 @@ class ChatRequest(BaseModel):
 def health():
     return {"status": "ok"}
 
+@app.post("/login")
+def login(req: LoginRequest):
+    return {"login_info": "This is a placeholder response from SEC Fault API."}
 
-@app.post("/chat")
-def chat(req: ChatRequest):
-    return {"reply": "This is a placeholder response from SEC Fault API."}
+@app.post("/signout")
+def signout(req: SignoutRequest):
+    return {"signout_info": "This is a placeholder response from SEC Fault API."}
+
+@app.post("/chats")
+def newChat(req: NewChatRequest):
+    return {"chat_info": "This is a placeholder response from SEC Fault API."}
+
+@app.get("/chats/{id}")
+def getChat(id: int):
+    return {"chat_and_msg_info": "This is a placeholder response from SEC Fault API."}
+
+@app.post("/chats/{id}/messages")
+def msg(id: int, req: NewMsgRequest):
+    return {"msg_reply": "This is a placeholder response from SEC Fault API."}
